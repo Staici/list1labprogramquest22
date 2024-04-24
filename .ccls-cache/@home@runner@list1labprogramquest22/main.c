7f28b28e28bb0abfe403c4e3272f8a6612017bf4
Steicie Aleixo Sousa <51095043+Staici@users.noreply.github.com>
2024-04-24 13:08:27 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@list1labprogramquest22/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+  int number;
+  printf("digite um numero");
+  scanf("%d", &number);  
+  printf("A letra digitada de acordo com a tabela ascii é %c: ",number);
+  
+    return 0;
+}
